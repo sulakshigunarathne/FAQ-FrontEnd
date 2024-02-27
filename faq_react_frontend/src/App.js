990009b103b75ad630a525675c3feddf03c0f2f3
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-02-27 15:16:57 +0530

    tailwind installed

--- faq_react_frontend/src/App.js
+++ faq_react_frontend/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header.tsx';
 
 function App() {
   return (
     <div className="App">
-    
+      <div>
+    <h1 className='text-6xl text-red-500'>Hello</h1>
+    <Header/>
+    </div>
     </div>
   );
 }
--- faq_react_frontend/src/Components/Header.tsx
+++ faq_react_frontend/src/Components/Header.tsx
@@ -1,3 +1,11 @@
+import React from 'react';
+
 const Header = () => {
-    
+    return(
+        <header className="header">
+            This is Header
+        </header>
+    );
 }
+
+export default Header;
