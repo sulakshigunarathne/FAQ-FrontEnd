7c6a0dd425b80ced363606b49f4492b3a5e68da2
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-02-28 12:25:36 +0530

    add new project

---
+++ FAQ-FrontEnd/faq_react_frontend/src/Components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyProfileComponent from './UserProfile';
+import logo from "../Assets/logo_main.png";
+const Header = () => {
+    return(
+        <header className="bg-dark-background sticky top-0 mx-auto flex w-full items-center justify-between border-b border-gray-500 p-8 ">
+            <img src = {logo} alt="logo" className="w-20 h-12 mr-2"/>
+            <h1 className="text-4xl font-bold ">QuePax</h1>
+            <MyProfileComponent/>
+            
+        </header>
+        
+    );
+}
+
+export default Header;
