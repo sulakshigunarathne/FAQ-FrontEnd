0b012eeaca53fd8a67f5fa429203951f16c1d47e
thushari-uop <thushariherath716@gmail.com>
2024-02-28 10:43:08 +0530

    Questions display UI

--- faq_react_frontend/src/App.js
+++ faq_react_frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Header from './Components/Header.tsx';
+import Questions from './Components/Questions.tsx';
 
 function App() {
   return (
     <div className="App">
       <div>
-    <h1 className='text-6xl text-red-500'>Hello</h1>
-    <Header/>
-    </div>
-    </div>
+        <h1 className='text-6xl text-red-500'>Hello</h1>
+        <Header/>
+      </div>
+      <Questions/>
+    
+    </div> 
   );
 }
 
---
+++ faq_react_frontend/src/Components/Questions.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {TETabs,TETabsContent,TETabsItem,TETabsPane} from "tw-elements-react";
+import Question from "./Question.tsx";
+
+export default function Questions(): JSX.Element {
+
+  const [fillActive, setFillActive] = useState("all");
+
+  const handleFillClick = (value: string) => {
+    if (value === fillActive) {
+      return;
+    }
+    setFillActive(value);
+  };
+
+  return (
+    <div className="mx-auto items-center content-center flex justify-center">
+      <div className="m-5 w-1/2 self-center">
+      <TETabs fill>
+        <TETabsItem
+          onClick={() => handleFillClick("all")}
+          active={fillActive === "all"}
+        >
+          All Questions
+        </TETabsItem>
+        <TETabsItem
+          onClick={() => handleFillClick("my")}
+          active={fillActive === "my"}
+        >
+          My Questions
+        </TETabsItem>
+        
+      </TETabs>
+
+      <TETabsContent>
+        <TETabsPane show={fillActive === "all"}>
+          <Question Que={"All q"}/>
+        </TETabsPane>
+        <TETabsPane show={fillActive === "my"}>
+          <Question Que={"My q"}/>
+        </TETabsPane>
+      </TETabsContent>
+    </div>
+
+    </div>
+    
+  );
+}
