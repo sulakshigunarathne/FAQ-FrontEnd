6548e1049011966f21ab1554421b5decc45f52f4
thushari-uop <thushariherath716@gmail.com>
2024-02-29 11:29:29 +0530

    Change the QuestionView

--- faq_fe_react/src/Pages/QuestionView.tsx
+++ faq_fe_react/src/Pages/QuestionView.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import {TETabs,TETabsContent,TETabsItem,TETabsPane} from "tw-elements-react";
-import Question from "../Components/Question";
+import {
+  TETabs,
+  TETabsContent,
+  TETabsItem,
+  TETabsPane,
+} from "tw-elements-react";
+import { useNavigate } from "react-router-dom";
 
 export default function QuestionView() {
-
   const [fillActive, setFillActive] = useState("all");
 
   const handleFillClick = (value: string) => {
@@ -13,36 +17,63 @@ export default function QuestionView() {
     setFillActive(value);
   };
 
+  const answerViewHandle = () => {
+    Navigate("/answer-view");
+  };
+
+  const Navigate = useNavigate();
+
   return (
     <div className="mx-auto items-center content-center flex justify-center">
       <div className="m-5 w-1/2 self-center">
-      <TETabs fill>
-        <TETabsItem
-          onClick={() => handleFillClick("all")}
-          active={fillActive === "all"}
-        >
-          All Questions
-        </TETabsItem>
-        <TETabsItem
-          onClick={() => handleFillClick("my")}
-          active={fillActive === "my"}
-        >
-          My Questions
-        </TETabsItem>
-        
-      </TETabs>
+        <TETabs fill>
+          <TETabsItem
+            onClick={() => handleFillClick("all")}
+            active={fillActive === "all"}
+          >
+            All Questions
+          </TETabsItem>
+          <TETabsItem
+            onClick={() => handleFillClick("my")}
+            active={fillActive === "my"}
+          >
+            My Questions
+          </TETabsItem>
+        </TETabs>
 
-      <TETabsContent>
-        <TETabsPane show={fillActive === "all"}>
-          <Question Que={"All q"}/>
-        </TETabsPane>
-        <TETabsPane show={fillActive === "my"}>
-          <Question Que={"My q"}/>
-        </TETabsPane>
-      </TETabsContent>
+        <TETabsContent>
+          <TETabsPane show={fillActive === "all"}>
+            <div className="mt-5 mx-auto items-center content-center flex justify-center">
+              <div className="flex justify-between self-center bg-gray-100 border rounded p-4 my-2 m-5 w-5/6">
+                <p className="">Question 1</p>
+                <div>
+                  <button
+                    className="px-2 py-2 w-auto bg-blue-500 text-white rounded mt-2 hover:bg-blue-600"
+                    onClick={answerViewHandle}
+                  >
+                    View Answer
+                  </button>
+                </div>
+              </div>
+            </div>
+          </TETabsPane>
+          <TETabsPane show={fillActive === "my"}>
+            <div className="mt-5 mx-auto items-center content-center flex justify-center">
+              <div className="flex justify-between self-center bg-gray-100 border rounded p-4 my-2 m-5 w-5/6">
+                <p className="">My Question 1</p>
+                <div>
+                  <button
+                    className="px-2 py-2 w-auto bg-blue-500 text-white rounded mt-2 hover:bg-blue-600"
+                    onClick={answerViewHandle}
+                  >
+                    View Answer
+                  </button>
+                </div>
+              </div>
+            </div>
+          </TETabsPane>
+        </TETabsContent>
+      </div>
     </div>
-
-    </div>
-    
   );
 }
